4725a3fca0575615b86f463fa0f1e59f622de7b5
Fantto00 <1531849260@qq.com>
2026-07-07 23:39:13 +0800

    refactor: replace MyUIPadding Builder with data class

--- my-ui/src/main/java/com/fantto/mylib/foundation/property/MyUIPadding.kt
+++ my-ui/src/main/java/com/fantto/mylib/foundation/property/MyUIPadding.kt
@@ -3,36 +3,20 @@ package com.fantto.mylib.foundation.property
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
-class MyUIPadding private constructor(
-    val top: Dp,
-    val bottom: Dp,
-    val start: Dp,
-    val end: Dp
+data class MyUIPadding(
+    val top: Dp = 0.dp,
+    val bottom: Dp = 0.dp,
+    val start: Dp = 0.dp,
+    val end: Dp = 0.dp
 ) {
     companion object {
         fun all(value: Dp): MyUIPadding =
             MyUIPadding(top = value, bottom = value, start = value, end = value)
 
         fun vertical(value: Dp): MyUIPadding =
-            MyUIPadding(top = value, bottom = value, start = 0.dp, end = 0.dp)
+            MyUIPadding(top = value, bottom = value)
 
         fun horizontal(value: Dp): MyUIPadding =
-            MyUIPadding(top = 0.dp, bottom = 0.dp, start = value, end = value)
-
-        fun builder() = Builder()
-    }
-
-    class Builder {
-        private var top: Dp = 0.dp
-        private var bottom: Dp = 0.dp
-        private var start: Dp = 0.dp
-        private var end: Dp = 0.dp
-
-        fun top(value: Dp) = apply { this.top = value }
-        fun bottom(value: Dp) = apply { this.bottom = value }
-        fun start(value: Dp) = apply { this.start = value }
-        fun end(value: Dp) = apply { this.end = value }
-
-        fun build() = MyUIPadding(top = top, bottom = bottom, start = start, end = end)
+            MyUIPadding(start = value, end = value)
     }
 }
